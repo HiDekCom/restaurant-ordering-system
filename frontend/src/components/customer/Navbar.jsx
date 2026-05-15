dcceb49538cf8d6471f90c54f7e56137ca58d911
am.kittiphon <am.kittiphon2002@gmail.com>
2026-05-15 22:59:01 +0700

    feat: navigate to cart page

--- frontend/src/components/customer/Navbar.jsx
+++ frontend/src/components/customer/Navbar.jsx
@@ -4,8 +4,12 @@ import { FaShoppingCart } from "react-icons/fa";
 
 import { CartContext } from "../../context/CartContext";
 
+import { useNavigate } from "react-router-dom";
+
 export default function Navbar() {
   const { cartItems } = useContext(CartContext);
+
+  const navigate = useNavigate();
 
   // รวมจำนวนสินค้า
   const totalQty = cartItems.reduce(
@@ -21,7 +25,11 @@ export default function Navbar() {
           QR Restaurant
         </h1>
 
-        <button className="relative">
+        {/* Cart Button */}
+        <button
+          onClick={() => navigate("/cart")}
+          className="relative"
+        >
           <FaShoppingCart className="text-2xl text-gray-700" />
 
           <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs px-2 py-1 rounded-full">
