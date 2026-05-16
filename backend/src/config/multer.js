7c7cfb22d04435fe1b385583c5bd55766a7f5a4b
am.kittiphon <am.kittiphon2002@gmail.com>
2026-05-17 02:31:00 +0700

    add cloudinary upload

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -1,22 +1,21 @@
 import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      Date.now() +
-        "-" +
-        file.originalname
-    );
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "restaurant",
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
   },
 });
 
-const upload = multer({
-  storage,
-});
+const upload = multer({ storage });
 
 export default upload;
